8f12a751814846cac36226ef01ac3e3c33294f52
jam-jam200 <jessieoyaks4j@gmail.com>
2023-09-03 20:10:47 +0100

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort {
         Scanner scanner = new Scanner(System.in);
 
         //get the user's input for the number of strings to be sorted
-        System.out.print("Enter the number of strings to be sorted: ");
+        System.out.print("Enter the number of strin gs to be sorted: ");
         int alphanum = scanner.nextInt();
         scanner.nextLine();
 
@@ -15,7 +15,7 @@ public class BubbleSort {
         //loop to get user input for strings
         for (int i = 0; i < alphanum; i++) {
             //prompting the user to enter a string with the current index
-            System.out.print("Enter string #" + (i + 1) + ": ");
+            System.out.print("Enter string: " + (i + 1) + ": ");
             strings[i] = scanner.nextLine();
         }
 
@@ -26,17 +26,18 @@ public class BubbleSort {
         System.out.print("Enter your choice: ");
         int sortOption = scanner.nextInt();
 
-        //sort the strings using Bubble Sort
+        //sorting the strings using Bubble Sort
         if (sortOption == 1) {
-            bubbleSort(strings, false); // Sort in alphanumeric order
+            bubbleSort(strings, false); //sorts in alphanumeric order
         } else if (sortOption == 2) {
-            bubbleSort(strings, true); // Sort in reverse alphanumeric order
+            bubbleSort(strings, true); //sorts in reverse alphanumeric order
         } else {
+            //in case the entered choice is invalid print out error message
             System.out.println("Invalid choice. Sorting in alphanumeric order by default.");
-            bubbleSort(strings, false); // Default to alphanumeric order
+            bubbleSort(strings, false); //default to alphanumeric order
         }
 
-        // Display the sorted strings
+        //displaying the sorted strings
         System.out.println("\nSorted Strings:");
         for (String str : strings) {
             System.out.println(str);
@@ -45,13 +46,21 @@ public class BubbleSort {
         scanner.close();
     }
 
-    // Bubble Sort
+    /**
+     * Sorts an array of strings using the Bubble Sort algorithm.
+     *
+     * @param arr The array of strings to be sorted.
+     * @param reverse If true, sorts the array in reverse alphanumeric order, otherwise, in alphanumeric order.
+     */
     public static void bubbleSort(String[] arr, boolean reverse) {
         int n = arr.length;
+        //outer loop for passes
         for (int i = 0; i < n - 1; i++) {
+            //inner loop for comparisons and swaps
             for (int j = 0; j < n - i - 1; j++) {
+                //comparing adjacent strings based on sorting order
                 if (compareStrings(arr[j], arr[j + 1], reverse) > 0) {
-                    // Swap arr[j] and arr[j+1]
+                    //swap arr[j] and arr[j+1]
                     String temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
